6b4fbc4e995a90a66c9453ea903c8debda222630
mauricio29 <mauricio@decisaosistemas.com.br>
2019-10-13 16:43:33 -0300

    primeiro commit

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
+import api from '../../services/api';
+
+import './styles.css';
+
+export default function Dashboard(){
+    const [spots, setSpots] = useState([]);
+
+    useEffect(() => {
+        const user_id = localStorage.getItem('user');
+        const socket = socketio('http://localhost:3333', {
+            query: { user_id },
+        });
+
+        socket.on('booking_request', data => {
+            console.log(data);
+        })
+    }, []);
+
+
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('./dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+        }
+
+        loadSpots();
+    }, []);
+
+    return (
+        <>
+            <ul className="spot-list">
+                { spots.map(spot => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
+                    </li>
+                ))}
+            </ul>
+
+            <Link to="/new">
+                <button className="btn">Cadastrar novo spot</button>
+            </Link>
+        </>            
+    )
+}
